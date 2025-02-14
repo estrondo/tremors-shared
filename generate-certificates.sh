282061b9281b7242ef6e8912e7a557ffec55bf53
rthoth! <ronaldo.asilva@gmail.com>
2025-02-14 08:09:51 -0300

    Update script and generated files.

--- generate-certificates.sh
+++ generate-certificates.sh
@@ -4,9 +4,15 @@
 echo "Okay friend, let's create the certificates to you be able to test tremors properly."
 
 OUT_DIR="server/ssl-certs"
+KEY_FILE="$OUT_DIR/tremors.key"
 PEM_FILE="$OUT_DIR/tremors.pem"
+CERT_FILE="$OUT_DIR/tremors.crt"
+
 SUBJ="/CN=estrondo.one/O=estrondo/C=BR"
+ALT_NAMES="DNS:*.estrondo.one"
+
+rm -f "$KEY_FILE" "$PEM_FILE" "$CERT_FILE"
 
 mkdir -p "$OUT_DIR"
-openssl req -new -nodes -keyout "$PEM_FILE" -subj $SUBJ -x509 -days 365 -out "$PEM_FILE" > /dev/null 2>&1
-chmod a=r "$PEM_FILE" > /dev/null 2>&1
+openssl req -new -nodes -keyout "$KEY_FILE" -subj $SUBJ -x509 -days 365 -out "$PEM_FILE" -addext "subjectAltName=$ALT_NAMES" > /dev/null 2>&1
+openssl x509 -outform der -in "$PEM_FILE" -out "$CERT_FILE" > /dev/null 2>&1
